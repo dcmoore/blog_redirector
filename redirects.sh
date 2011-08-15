29d77224c1b81746fafada73af6e39701eb623d2
Dave Moore <dcmoore@Dave-Moores-MacBook-Pro.local>
2011-08-15 12:24:48 -0500

    Implemented a method that creates a specified directory structure and creates a redirect html file at the end of each directory.

--- redirects.sh
+++ redirects.sh
@@ -14,13 +14,3 @@ make_new_directory_and_add_redirect()
 	print_redirect_script $2 > index.html
 	cd "../../../../../"
 }
-
-arr_of_old_paths=( "articles/2011/6/23/type-camp-california-day-two-recap" "articles/2011/6/20/type-camp-california-day-one-recap" "articles/2011/6/11/winning-at-consistency" )
-arr_of_new_paths=( "http://8thlight.github.com/billy-whited/2011/06/23/type-camp-california-day-2.html" "http://8thlight.github.com/billy-whited/2011/06/20/type-camp-california-day-1.html" "http://8thlight.github.com/colin-jones/2011/06/10/winning-at-consistency.html")
-i=0
-
-for arr_of_old_paths in ${arr_of_old_paths[@]}
-do
-	make_new_directory_and_add_redirect $arr_of_old_paths ${arr_of_new_paths[$i]}
-	i=`expr $i + 1`
-done
